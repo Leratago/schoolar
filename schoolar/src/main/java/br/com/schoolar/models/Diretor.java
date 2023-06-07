bd86efad7ec771a1020c2931373d897cb34e8b87
Gabriel Marques <gabrielmarques409@gmail.com>
2023-06-06 21:13:35 -0300

    hehe boy

--- schoolar/src/main/java/br/com/schoolar/models/Diretor.java
+++ schoolar/src/main/java/br/com/schoolar/models/Diretor.java
@@ -1,4 +1,23 @@
 package br.com.schoolar.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "diretor")
 public class Diretor {
+
+    @Id
+    private String nome;
+    private String datadenascimento;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private String departamento;
+    private String equipedegestao;
 }
